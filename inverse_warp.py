d5f49e8670b3708c7f8246335382f38dc26efbe0
xdr940 <xdr94@126.com>
2019-05-15 17:23:14 +0800

    upd

--- inverse_warp.py
+++ inverse_warp.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import torch
 import torch.nn.functional as F
-
+import matplotlib.pyplot as plt
 pixel_coords = None
 
 
@@ -12,7 +12,7 @@ def set_id_grid(depth):
     j_range = torch.arange(0, w).view(1, 1, w).expand(1,h,w).type_as(depth)  # [1, H, W]
     ones = torch.ones(1,h,w).type_as(depth)
 
-    pixel_coords = torch.stack((j_range, i_range, ones), dim=1)  # [1, 3, H, W]
+    pixel_coords = torch.stack((j_range, i_range, ones), dim=1)  # [1, 3, H, W] 常量定义，一直不变,完全可以不定义，但是为了可读性
 
 
 def check_sizes(input, input_name, expected):
@@ -24,7 +24,7 @@ def check_sizes(input, input_name, expected):
 
 
 def pixel2cam(depth, intrinsics_inv):#像素坐标2相机坐标
-    global pixel_coords#1,3,128,416 loss func 在外层正对每个ref-img遍历呢
+    global pixel_coords#1,3,128,416  这个3不是通道，是坐标， lossfunc 在外层正对每个ref-img遍历
     """Transform coordinates in the pixel frame to the camera frame.
     Args:
         depth: depth maps -- [B, H, W]
@@ -39,10 +39,22 @@ def pixel2cam(depth, intrinsics_inv):#像素坐标2相机坐标
     #tmp2=tmp1.expand(b,3,h,w)
     #tmp3=tmp2.reshape(b, 3, -1)#[]
     current_pixel_coords = pixel_coords[:,:,:h,:w].expand(b,3,h,w).reshape(b, 3, -1)# [B, 3, H*W]
-    TMP=intrinsics_inv @ current_pixel_coords
+    TMP=intrinsics_inv @ current_pixel_coords#投影矩阵逆乘,得到camcoords
     #[B,3,3]@[B,3,H*W]=[B,3,H*W]#大概乘法逻辑懂了，但是原理逻辑不太懂
     #@符号是矩阵想成，要求两个tensor order一样，并且后两介满足惩罚要求，并且除了后两阶其余维度完全一致
     cam_coords = (TMP).reshape(b, 3, h, w)
+    """
+    print(pixel_coords[0,:,0,0])#value: 0,0,1
+    print(pixel_coords[0,:,1,0])#1,0,1
+    print(pixel_coords[0,:,0,1])#0,1,1
+    print(pixel_coords[0,:,1,1])#1,1,1
+
+    print(cam_coords[0,:,0,0])#-.8650,-.2286
+    print(cam_coords[0,:,0,1])#-.8612,-.2286
+    print(cam_coords[0,:,1,0])#-.8650,-.2249
+    print(cam_coords[0,:,1,1])#-.8612,-.2249
+    
+    """
     #cam_coords:B,3,128,416
     return cam_coords * depth.unsqueeze(1)#[B,3,H,W]X[B,1,H,W]=[B,3,H,W]
 
@@ -50,14 +62,28 @@ def pixel2cam(depth, intrinsics_inv):#像素坐标2相机坐标
 def cam2pixel(cam_coords, proj_c2p_rot, proj_c2p_tr, padding_mode):
     """Transform coordinates in the camera frame to the pixel frame.
     Args:
-        cam_coords: pixel coordinates defined in the first camera coordinates system -- [B, 4, H, W]
+        cam_coords: pixel coordinates defined in the first camera coordinates system -- [B, 4, H, W]??明明是[B,3,H,W]
         proj_c2p_rot: rotation matrix of cameras -- [B, 3, 4]
         proj_c2p_tr: translation vectors of cameras -- [B, 3, 1]
     Returns:
         array of [-1,1] coordinates -- [B, 2, H, W]
     """
     b, _, h, w = cam_coords.size()
-    cam_coords_flat = cam_coords.reshape(b, 3, -1)  # [B, 3, H*W]
+
+    """
+    print(cam_coords[0,:,0,0])
+    print(cam_coords[0,:,0,1])
+    print(cam_coords[0,:,1,0])
+    print(cam_coords[0,:,1,1])
+
+    
+    tensor([-0.1386, -0.0366,  0.1602], device='cuda:0', grad_fn=<SelectBackward>)
+    tensor([-0.1193, -0.0317,  0.1386], device='cuda:0', grad_fn=<SelectBackward>)
+    tensor([-0.1298, -0.0338,  0.1501], device='cuda:0', grad_fn=<SelectBackward>)
+    tensor([-0.1263, -0.0330,  0.1466], device='cuda:0', grad_fn=<SelectBackward>)
+    已经变得立体了
+    """
+    cam_coords_flat = cam_coords.reshape(b, 3, -1)  # [B, 3, H*W]#这个意思：本来每个格子有三个值，所有格子组成一个矩阵，现在吧矩阵拉成一条array操作
     if proj_c2p_rot is not None:
         pcoords = proj_c2p_rot @ cam_coords_flat
     else:
@@ -68,12 +94,20 @@ def cam2pixel(cam_coords, proj_c2p_rot, proj_c2p_tr, padding_mode):
     X = pcoords[:, 0]
     Y = pcoords[:, 1]
     Z = pcoords[:, 2].clamp(min=1e-3)
+    """
+    >>> a = torch.randn(4)
+    >>> a
+    tensor([-1.7120,  0.1734, -0.0478, -0.0922])
+    >>> torch.clamp(a, min=-0.5, max=0.5)
+    tensor([-0.5000,  0.1734, -0.0478, -0.0922])
+    
+    """
 
     X_norm = 2*(X / Z)/(w-1) - 1  # Normalized, -1 if on extreme left, 1 if on extreme right (x = w-1) [B, H*W]
     Y_norm = 2*(Y / Z)/(h-1) - 1  # Idem [B, H*W]
 
     pixel_coords = torch.stack([X_norm, Y_norm], dim=2)  # [B, H*W, 2]
-    return pixel_coords.reshape(b,h,w,2)
+    return pixel_coords.reshape(b,h,w,2)#变成比较容易理解的二维矩阵，每个格子两个值-,x,y
 
 
 def euler2mat(angle):
@@ -160,7 +194,7 @@ def pose_vec2mat(vec, rotation_mode='euler'):
 
 
 def inverse_warp(img, depth, pose, intrinsics, rotation_mode='euler', padding_mode='zeros'):
-    """
+    """#比较重要的一个函数,本函数所有的img都是一张ref/src img
     Inverse warp a source image to the target image plane.# article [8]
 
     Args:
@@ -175,21 +209,25 @@ def inverse_warp(img, depth, pose, intrinsics, rotation_mode='euler', padding_mo
     check_sizes(img, 'img', 'B3HW')
     check_sizes(depth, 'depth', 'BHW')
     check_sizes(pose, 'pose', 'B6')
-    check_sizes(intrinsics, 'intrinsics', 'B33')
+    check_sizes(intrinsics, 'intrinsics', 'B33')#本来3x4,但是第四列有一个基线参数，被去掉了，成了3x3
 
     batch_size, _, img_height, img_width = img.size()
 
+                            #pixel_coords也使用了(这是个常量！量等于坐标)， 通过深度图和像素坐标，得到相机坐标
     cam_coords = pixel2cam(depth, intrinsics.inverse())  # [B,3,H,W]2
 
     pose_mat = pose_vec2mat(pose, rotation_mode)  # [B,3,4]
 
     # Get projection matrix for tgt camera frame to source pixel frame
-    proj_cam_to_src_pixel = intrinsics @ pose_mat  # [B, 3, 4]
+    proj_cam_to_src_pixel = intrinsics @ pose_mat  # [B, 3, 4]#内参x外参
 
-    rot, tr = proj_cam_to_src_pixel[:,:,:3], proj_cam_to_src_pixel[:,:,-1:]
+    rot, tr = proj_cam_to_src_pixel[:,:,:3], proj_cam_to_src_pixel[:,:,-1:]#得到R,t ，这里只是为了简化，R[B,3,3], t[b,3,1]
     src_pixel_coords = cam2pixel(cam_coords, rot, tr, padding_mode)  # [B,H,W,2]
-    projected_img = F.grid_sample(img, src_pixel_coords, padding_mode=padding_mode)
+                                 #[B,3,H,W]          [B,H,W,2]
+    projected_img = F.grid_sample(input=img, grid = src_pixel_coords, padding_mode=padding_mode)#[B,3,H,W]
 
     valid_points = src_pixel_coords.abs().max(dim=-1)[0] <= 1
-
+#    tes=projected_img.cpu().data.numpy()[0]
+ #   plt.imshow(tes)
+ #   plt.show()
     return projected_img, valid_points
